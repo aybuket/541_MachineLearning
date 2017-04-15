17725d629b24ae5286ea6769129035e7b5d4c1bc
aybuket <aakgul13@ku.edu.tr>
2017-04-15 03:00:18 +0300

    Second

--- README.md
+++ README.md
@@ -3,5 +3,15 @@
 Orijinal Paper: 
 Ronghang Hu, Marcus Rohrbach, Jacob Andreas, Trevor Darrell, Kate Saenko, 2016, Modeling Relationships in Referential Expressions with Compositional Modular Networks, Web, url: https://arxiv.org/abs/1611.09978
 
+Aim: 
+Replication of the above paper as a machine learning class ter project
+
 Summary:
 Localization of referential expressions in an image 
+
+Abstract:
+Generally, when we are talking about some people or some object, we refer them accordingly their relationship
+with the other objects or people. However, if we, as a human, understand objects or people with these
+relationships, then computers also must be able to understand this way so that we can have more accurate
+results. In this paper, I use Computational Modular Networks (CMNs) which first parses the referential
+expression as entities and relations,then identifies in the image and finds the most related one.
--- TermProject.jl
+++ TermProject.jl
@@ -1,4 +1,7 @@
 using PyCall
+@pyimport GloVe
+using Knet
+
 # Pycall to use GloVe
 
 # Train 3000000 iteration
@@ -9,6 +12,16 @@ using PyCall
 global momentum = 0.95;
 global lr = 0.005;
 
+# Shape extract
+
+
+# Visual Genome extract
+
+# Visual 7W extract
+
+# GoogleRef extract
+
+
 
 #####################################
 # Expression parsing with attention #
@@ -16,19 +29,43 @@ global lr = 0.005;
 
 # Sequence of T words {wt}
 # Embed each word wt to a vector et using GloVe
+function parser(w)
+  #embedding with GloVe
+  e = glove(w)
+  ht = lstm(e)
+  asubj = attention()
+  aobj = attention()
+  arel = attention()
+  qsubj = languageRep(asubj,e)
+  qobj = languageRep(aobj,e)
+  qrel = languageRep(arel,e)
+end
 # Scan through the {et} with a 2-layer bidirectonal LSTM
 # LTSM = 1000-dim hidden state, ht = 4000-dim
 # First layer of LSTM = input: {et}, output: forward hidden state ht(1,fw)
 # and backward hidden state ht(1,bw) at each time step. Becomes ht(1)
 # Second layer of LSTM = input: {ht(1)}, output: ht(2,fw) & ht(2,bw)
-# Then ht = [ht(1,fw), ht(1,fw), ht(2,fw), ht(2,bw)]
-# Attention calculation (subj as ex., rel and obj is same)
-# a(t,subj) = exp(B(T,subj).ht) / ∑(µ=1,T) exp(B(T,subj).hµ)
-function lstm()
+# Then ht = [ht(1,fw), ht(1,bw), ht(2,fw), ht(2,bw)]
+function lstmLayer1(e)
   for h=1:1000
   end
+  return (ht1fw,ht1bw)
 end
 
+function lstmLayer2(ht)
+
+  return (ht2fw, ht2bw)
+end
+function lstm(e)
+  (ht1fw,ht1bw) = lstmLayer1(e)
+  ht1 = hcat(ht1fw,ht1bw)
+  (ht2fw, ht2bw) = lstmLayer2(ht1)
+  ht2 = hcat(ht2fw, ht2bw)
+  ht = hcat(ht1,ht2)
+  return ht
+end
+# Attention calculation (subj as ex., rel and obj is same)
+# a(t,subj) = exp(B(T,subj).ht) / ∑(µ=1,T) exp(B(T,subj).hµ)
 function attention(b,ht)
   expValue = exp(b' * ht)
   a = expValue/sum(expValue)
@@ -57,22 +94,20 @@ end
 # Xspatial = [Xmin/WI, Ymin/HI, Xmax/WI, Ymax/HI, Sb/SI],
 # where [xmin, ymin, xmax, ymax] and Sb are bounding box coordinates and area of b
 # and WI width, HI height and SI are of the image I.
-# Xv,s = [Xv, Xs] = representation of region b
-
-# ~Xv,s = Wv,s*Xv,s + bv,s
-# zloc = ~Xv,s .* qloc
-# ^zloc = zloc / ||zloc||
-
-# Prediction sloc = wTloc*^zloc+bloc
 
 # The parameters in QLoc = (Wv,s, bv,s, wloc, bloc)
 function locationModule(b,qloc,lw)
   xs = 0 # spatial of b
   xv = 0 # visual of b with conv ?!?!?!!?!?!?!?!?!
+  # Xv,s = [Xv, Xs] = representation of region b
   xvs = [xv, xs]
+  # ~Xv,s = Wv,s*Xv,s + bv,s
   xhat = lw[1]*xvs + lw[2]
+  # zloc = ~Xv,s .* qloc
   zloc = xhat .* qloc
+  # ^zloc = zloc / ||zloc||
   zhat = zloc / length(zloc)
+  # Prediction sloc = wTloc*^zloc+bloc
   sloc = lw[3]' * zhat + lw[4]
   return sloc
 end
@@ -81,24 +116,22 @@ end
 # Relationship Module #
 #######################
 
-# Xspatial = [Xmin/WI, Ymin/HI, Xmax/WI, Ymax/HI, Sb/SI],
-# where [xmin, ymin, xmax, ymax] and Sb are bounding box coordinates and area of b
-# and WI width, HI height and SI are of the image I.
-# x1,x2 are spatial features of b1 and b2
-# x1,2 = [x1,x2]
-# ~x1,2 = W1,2*x1,2 + b1,2
-# zrel = ~x1,2 .* Qrel
-# ^zrel = zrel / ||zrel||
-# srel = wTrel * ^zrel + brel
 # The parameters in QLoc = (W1,2, b1,2, wrel, brel)
-
 function relationModule(b1,b2,qrel,rw)
+  # Xspatial = [Xmin/WI, Ymin/HI, Xmax/WI, Ymax/HI, Sb/SI],
+  # where [xmin, ymin, xmax, ymax] and Sb are bounding box coordinates and area of b
+  # and WI width, HI height and SI are of the image I.
   x1 = 0 # spatial features of b1
   x2 = 0 # spatial features of b2
+  # x1,2 = [x1,x2]
   x = [x1, x2]
+  # ~x1,2 = W1,2*x1,2 + b1,2
   xhat = rw[1]*x + rw[2]
+  # zrel = ~x1,2 .* Qrel
   zrel = xhat .* qrel
+  # ^zrel = zrel / ||zrel||
   zhat = zrel / length(zrel)
+  # srel = wTrel * ^zrel + brel
   srel = rw[3]'*zhat + rw[4]
   return srel
 end
@@ -127,7 +160,16 @@ function weakloss()
 end
 
 function train()
+  for (x,y) in data
+  end
+
 end
 
-function accuracy()
+function precision()
+  trueCount = 0
+  count = 0
+  for (x,y) in data
+  end
+  result = trueCount / count
+  return result
 end
